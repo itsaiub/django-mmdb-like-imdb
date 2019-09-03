51acdb96d72af3f44cb4bafe9d499acc6eb7f647
itsaiub <itsaiub@gmail.com>
2019-09-03 10:04:28 +0600

    add Role and Person Model

--- core/models.py
+++ core/models.py
@@ -22,6 +22,12 @@ class Movie(models.Model):
     rating = models.IntegerField(choices=RATINGS, default=NOT_RATED)
     runtime = models.PositiveIntegerField()
     website = models.URLField(blank=True)
+    director = models.ForeignKey(
+        to='Person', on_delete=models.SET_NULL, related_name='directed', blank=True, null=True)
+    writers = models.ManyToManyField(
+        to='Person', related_name='writing_credits', blank=True)
+    actors = models.ManyToManyField(
+        to='Person', through='Role', related_name='acting_credits', blank=True)
 
     # descending order in year, its like ORDER BY year DESC, title
     class Meta:
@@ -29,3 +35,32 @@ class Movie(models.Model):
 
     def __str__(self):
         return f'{self.title} ({self.year})'
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey('Person', on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return f'{self.movie_id} {self.person_id} {self.name}'
+
+    class Meta:
+        unique_together = ('movie', 'person', 'name')
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=140)
+    last_name = models.CharField(max_length=140)
+    born = models.DateField()
+    died = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = (
+            'last_name', 'first_name'
+        )
+
+    def __str__(self):
+        if self.died:
+            return f'{self.last_name}, {self.first_name} ({self.born}-{self.died})'
+        return f'{self.last_name}, {self.first_name} ({self.born})'
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.test.client import \
+    RequestFactory
+from django.urls.base import reverse
+
+from core.models import Movie
+from core.views import MovieList
+
+
+class MovieListPaginationTestCase(TestCase):
+    ACTIVE_PAGINATION_HTML = """
+      <li class='page-item active'>
+        <a href='{}?page={}' class='page-link'>{}</a>
+      </li>
+    """
+
+    def setUp(self):
+        for n in range(15):
+            Movie.objects.create(
+                title='Title {}'.format(n),
+                year=1990 + n,
+                runtime=100,
+            )
+
+    def testFirstPage(self):
+        movie_list_path = reverse('core:MovieList')
+        request = RequestFactory().get(path=movie_list_path)
+        reponse = MovieList.as_view()(request)
+        self.assertEquals(200, reponse.status_code)
+        self.assertTrue(reponse.context_data['is_paginated'])
+        self.assertInHTML(
+            self.ACTIVE_PAGINATION_HTML.format(
+                movie_list_path, 1, 1
+            ),
+            reponse.rendered_content
+        )
