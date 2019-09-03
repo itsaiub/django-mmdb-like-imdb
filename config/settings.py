37f77e121add961901ec44a7f4099ea6674ea16a
itsaiub <itsaiub@gmail.com>
2019-09-03 11:54:10 +0600

    user register, login and logout

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL = 'core:MovieList'
+LOGOUT_REDIRECT_URL = 'core:MovieList'
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import urls, views as auth_views
+
 from . import views
 
 app_name = 'user'
 
 urlpatterns = [
-    # path('movies/', views.MovieList.as_view(), name='MovieList'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ]
