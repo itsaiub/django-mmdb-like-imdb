d1f13550c28ed9effcc3c72e7db0616e1e786fea
itsaiub <itsaiub@gmail.com>
2019-09-04 00:15:23 +0600

    configure file upload

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'core:MovieList'
 LOGOUT_REDIRECT_URL = 'core:MovieList'
+
+
+MEDIA_URL = '/uploaded/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
