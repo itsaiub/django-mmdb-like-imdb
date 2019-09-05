366092e758e4e3d90b90ac9e5648ba87ded8ccf9
itsaiub <itsaiub@gmail.com>
2019-09-05 11:38:59 +0600

    caching objects

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+import django
 from django.views.generic import (
     ListView, DetailView, CreateView, UpdateView
 )
@@ -6,6 +7,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from django.core.exceptions import PermissionDenied
+from django.core.cache import cache
 
 from core.models import Movie, Person, Vote
 from core.forms import VoteForm, MovieImageForm
@@ -16,7 +18,21 @@ from core.mixins import CachePageVaryOnCookieMixin
 
 class TopMovies(ListView):
     template_name = 'core/top_movies_list.html'
-    queryset = Movie.objects.top_movies(limit=10)
+
+    def get_queryset(self):
+        limit = 10
+        key = 'top_movies_%s' % limit
+        cached_qs = cache.get(key)
+
+        if cached_qs:
+            same_django = cached_qs._django_version == django.get_version()
+            if same_django:
+                return cached_qs
+        qs = Movie.objects.top_movies(
+            limit=limit
+        )
+        cache.set(key, qs)
+        return qs
 
 
 class MovieList(CachePageVaryOnCookieMixin, ListView):
